d257ff48862ab413d85005da9a0bd63c559d6071
takaishotaro <takaishotaro326@gmail.com>
2022-11-12 23:18:20 +0900

    update loadbalancer

--- level2/instance.tf
+++ level2/instance.tf
@@ -7,7 +7,8 @@ resource "aws_instance" "yourdevops-private-server" {
   ami                    = "ami-0de5311b2a443fb89"
   instance_type          = "t2.micro"
   subnet_id              = data.terraform_remote_state.level1.outputs.private_subnet_id[0]
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  vpc_security_group_ids = [aws_security_group.private_sg.id]
+  user_data              = file("user-data.sh")
 
   key_name = aws_key_pair.keypair.key_name
 
@@ -20,9 +21,8 @@ resource "aws_instance" "yourdevops-public-server" {
   ami                         = "ami-0de5311b2a443fb89"
   instance_type               = "t2.micro"
   subnet_id                   = data.terraform_remote_state.level1.outputs.public_subnet_id[0]
-  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  vpc_security_group_ids      = [aws_security_group.public_sg.id]
   associate_public_ip_address = true
-  user_data                   = file("user-data.sh")
 
   key_name = aws_key_pair.keypair.key_name
 
---
+++ level2/load_balancer.tf
@@ -0,0 +1,46 @@
+resource "aws_lb" "main" {
+  name = var.env_code
+  internal = false
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.load_balancer.id]
+  subnets = data.terraform_remote_state.level1.outputs.public_subnet_id
+
+  tags = {
+    Name = var.env_code
+  }
+}
+
+resource "aws_lb_target_group" "main" {
+  name = var.env_code
+  port = 80
+  protocol = "HTTP"
+  vpc_id = data.terraform_remote_state.level1.outputs.vpc_id
+
+  health_check {
+    enabled = true
+    path = "/"
+    port = "traffic-port"
+    healthy_threshold = 5
+    unhealthy_threshold = 2
+    timeout = 5
+    interval = 30
+    matcher = 200
+  }
+}
+
+resource "aws_lb_target_group_attachment" "main" {
+  target_group_arn = aws_lb_target_group.main.arn
+  target_id = aws_instance.yourdevops-private-server.id
+  port = 80
+}
+
+resource "aws_lb_listener" "main" {
+    load_balancer_arn = aws_lb.main.arn
+    port = "80"
+    protocol = "HTTP"
+
+    default_action {
+      type = "forward"
+      target_group_arn = aws_lb_target_group.main.arn
+    }
+}
