4451f4a6f05c0d71c68f87bf6d3d0fc0c3603781
takaishotaro <takaishotaro326@gmail.com>
2022-11-16 23:13:46 +0900

    fixed code style of iam.tf

--- level2/iam.tf
+++ level2/iam.tf
@@ -1,9 +1,7 @@
-
-
 resource "aws_iam_role" "main" {
-  name               = var.env_code
+  name                = var.env_code
   managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"]
-  assume_role_policy = <<EOF
+  assume_role_policy  = <<EOF
 {
     "Version": "2012-10-17",
     "Statement":[
@@ -20,7 +18,6 @@ resource "aws_iam_role" "main" {
 }
 EOF
 }
-
 
 resource "aws_iam_instance_profile" "main" {
   name = var.env_code
