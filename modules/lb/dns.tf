72fd2736d2317c890f230c378062d77953b0a165
takaishotaro <takaishotaro326@gmail.com>
2022-11-18 21:48:52 +0900

    fmt file

--- modules/lb/dns.tf
+++ modules/lb/dns.tf
@@ -4,8 +4,8 @@ data "aws_route53_zone" "main" {
 
 resource "aws_route53_record" "www" {
   zone_id = data.aws_route53_zone.main.zone_id
-  name = "www.${data.aws_route53_zone.main.name}"
-  type = "CNAME"
-  ttl = "300"
-  records = [ aws_lb.main.dns_name ]
+  name    = "www.${data.aws_route53_zone.main.name}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_lb.main.dns_name]
 }
